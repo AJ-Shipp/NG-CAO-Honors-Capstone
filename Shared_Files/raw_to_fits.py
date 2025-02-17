81edb0803b36eb58516ea2ca4e877185d3e3165b
AJ-Shipp <anthonyjamesshipp@gmail.com>
2025-02-17 10:36:31 -0600

    Update raw_to_fits.py
    - Added a 3 choice system using the "transform" value corresponding to the different ways to change from a .Raw to .fits
    - Ran all "Testing_2-10" images through the program

--- Shared_Files/raw_to_fits.py
+++ Shared_Files/raw_to_fits.py
@@ -2,19 +2,53 @@ from astropy.io import fits
 import numpy as np
 import os
 
+transform = 2
+
 if __name__ == '__main__':
 
-    filename = r"C:\Users\antho\Videos\NG\Testing_2-7\test1_1d7ND.raw"
-    # "C:\Users\antho\Videos\NG\PSF_Characterization\p_0-0\pos_ZeroZero-0.Raw"
+    filename = r"C:\Users\antho\Videos\NG\Testing_2-10\PSFdata_ff\ff_all\dark_0-0_-0210202510"
 
-    for j in range(0,30):
-        raw_file1 = filename #+ str(j) + '.Raw'
+    if transform == 0:
+        for i in range(0,999999):
+            raw_file1 = filename + str(i)
+            for j in range(0,99): 
+                raw_file1 = raw_file1 + '-' + str(j) + '.raw'
 
-        if os.path.isfile(raw_file1):
-            raw_imarray = np.fromfile(raw_file1, dtype='int16')
-            reshaped_raw_imarray = np.reshape(raw_imarray, (1944,2592))
-            fits_file = raw_file1.split('.')[0]+'.fits'
-            hdu = fits.ImageHDU(reshaped_raw_imarray)
-            prim = fits.PrimaryHDU()
-            hdul = fits.HDUList([prim,hdu])
-            hdu.writeto(fits_file, overwrite=True)
+                if os.path.isfile(raw_file1):
+                    raw_imarray = np.fromfile(raw_file1, dtype='int16')
+                    reshaped_raw_imarray = np.reshape(raw_imarray, (1944,2592))
+                    fits_file = raw_file1.split('.')[0]+'.fits'
+                    hdu = fits.ImageHDU(reshaped_raw_imarray)
+                    prim = fits.PrimaryHDU()
+                    hdul = fits.HDUList([prim,hdu])
+                    hdu.writeto(fits_file, overwrite=True)
+
+    if transform == 1:
+        for j in range(0,40):
+            raw_file1 = filename #+ str(j) + '.Raw'
+
+            if os.path.isfile(raw_file1):
+                raw_imarray = np.fromfile(raw_file1, dtype='int16')
+                reshaped_raw_imarray = np.reshape(raw_imarray, (1944,2592,2))
+                fits_file = raw_file1.split('.')[0]+'.fits'
+                hdu = fits.ImageHDU(reshaped_raw_imarray[:,:,0])
+                prim = fits.PrimaryHDU()
+                hdul = fits.HDUList([prim,hdu])
+                hdu.writeto(fits_file, overwrite=True)
+
+    if transform == 2:
+        for i in range(0,9999):
+            raw_file1 = filename + str(i) + '-'
+            for j in range(0,40):
+                raw_file2 = raw_file1 + str(j) + '.Raw'
+
+                if os.path.isfile(raw_file2):
+                    raw_imarray = np.fromfile(raw_file2, dtype='int16')
+                    reshaped_raw_imarray = np.reshape(raw_imarray, (1944,2592,2))
+                    fits_file = raw_file2.split('.')[0]+'.fits'
+                    hdu = fits.ImageHDU(reshaped_raw_imarray[:,:,0])
+                    prim = fits.PrimaryHDU()
+                    hdul = fits.HDUList([prim,hdu])
+                    hdu.writeto(fits_file, overwrite=True)
+
+print(fits_file)
